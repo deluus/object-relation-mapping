39f4a254615b63c981a773371d1333170cf125df
deluus polycarp <pdeluus@gmail.com>
2022-03-14 17:53:27 -0700

    updated routes. troubleshoot and found issues with connection to insominia. created update routes, delete routes. cleaned code, tested. created insomnia requests

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,23 +1,28 @@
 const router = require('express').Router();
-const { where } = require('sequelize/types');
 const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // The `/api/products` endpoint
 
 // get all products
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  // try {
+  //   const productData = await Product.findAll();
+  //   res.status(200).json(productData);
+  // } catch (err) {
+  //   res.status(500).json(err);
+  // }
 
   Product.findAll({
     include: [
       {
         model: Category,
-        attributes: ["category_name"],
+        attributes: ['category_name'],
       },
       {
         model: Tag,
-        attributes: ["Tags_name"],
+        attributes: ['tag_name'],
       },
     ],
   })
@@ -30,9 +35,24 @@ router.get("/", (req, res) => {
 });
 
 // get one product
-router.get("/:id", (req, res) => {
+router.get("/:id", async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  // try {
+  //   const productData = await Product.findByPk(req.params.id, {
+  //     // JOIN with catergory, using the Tag through table
+  //     include: [{ model: Category, through: Tag, as: 'Category_Tags' }]
+  //   });
+
+  //   if (!productData) {
+  //     res.status(404).json({ message: 'No oproduct found with this id!' });
+  //     return;
+  //   }
+
+  //   res.status(200).json(productData);
+  // } catch (err) {
+  //   res.status(500).json(err);
+  // }
   Product.findOne({
     where: {
       id: req.params.id,
@@ -40,11 +60,11 @@ router.get("/:id", (req, res) => {
     include: [
       {
         model: Category,
-        attributes: ["category_name"],
+        attributes: ['category_name'],
       },
       {
         model: Tag,
-        attributes: ["Tag_name"],
+        attributes: ['Tag_name'],
       },
     ],
   })
@@ -56,7 +76,7 @@ router.get("/:id", (req, res) => {
 });
 
 // create new product
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   console.log(" post route to create new product");
 
   // /* req.body should look like this...
@@ -67,6 +87,12 @@ router.post("/", (req, res) => {
   //     tagIds: [1, 2, 3, 4]
   //   }
   // */
+  // }
+  // try {
+  //   const productData = await Product.create(req.body);
+  //   res.status(200).json(productData);
+  // } catch (err) {
+  //   res.status(400).json(err);
   // }
 
   Product.create(req.body)
@@ -92,8 +118,8 @@ router.post("/", (req, res) => {
 });
 
 // update product
-router.put("/:id", (req, res) => {
-  // update product data
+router.put("/:id", async (req, res) => {
+  //update product data
   Product.update(req.body, {
     where: {
       id: req.params.id,
@@ -128,13 +154,29 @@ router.put("/:id", (req, res) => {
     })
     .then((updatedProductTags) => res.json(updatedProductTags))
     .catch((err) => {
-      // console.log(err);
+      console.log(err);
       res.status(400).json(err);
     });
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id",async (req, res) => {
   // delete one product by its `id` value
+  // try {
+  //   const productData = await Product.destroy({
+  //     where: {
+  //       id: req.params.id
+  //     }
+  //   });
+
+  //   if (!productData) {
+  //     res.status(404).json({ message: 'No product found with this id!' });
+  //     return;
+  //   }
+
+  //   res.status(200).json(productData);
+  // } catch (err) {
+  //   res.status(500).json(err);
+  // }
   Product.destroy({
     where: {
       id: req.params.id,
